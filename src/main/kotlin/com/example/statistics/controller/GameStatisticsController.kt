0672f67d44c019cdc2417f3191ab27945468967b
Sade <kinner@th-brandenburg.de>
2024-01-10 15:37:59 +0100

    cleaner code

--- src/main/kotlin/com/example/statistics/controller/GameStatisticsController.kt
+++ src/main/kotlin/com/example/statistics/controller/GameStatisticsController.kt
@@ -3,9 +3,7 @@ package com.example.statistics.controller
 import com.example.statistics.service.GameResult
 import com.example.statistics.service.GameStatisticsService
 import org.springframework.web.bind.annotation.*
-/*import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController*/
+
 
 @RestController
 @RequestMapping("/statistics")
@@ -21,7 +19,6 @@ class GameStatisticsController(private val gameStatisticsService: GameStatistics
         if (isWinner) {
             gameStatisticsService.updateStatistics(true)
         } else {
-            // Hier wird die Statistik nur aktualisiert, wenn das Spiel verloren wurde
             gameStatisticsService.updateStatistics(false)
         }
     }
--- src/main/kotlin/com/example/statistics/service/GameStatisticsService.kt
+++ src/main/kotlin/com/example/statistics/service/GameStatisticsService.kt
@@ -2,7 +2,6 @@ package com.example.statistics.service
 
 import org.springframework.stereotype.Service
 
-
 data class GameResult(val totalGames: Int, val wins: Int, val losses: Int, val winPercentage: Double)
 
 @Service
@@ -19,32 +18,8 @@ class GameStatisticsService {
             losses++
         }
     }
-
     fun getStatistics(): GameResult {
         val winPercentage = if (totalGames > 0) (wins.toDouble() / totalGames) * 100 else 0.0
         return GameResult(totalGames, wins, losses, winPercentage)
     }
 }
-
-/*
-@Service
-class GameStatisticsService {
-
-    private var wins: Int = 0
-    private var losses: Int = 0
-
-    fun updateStatistics(isWinner: Boolean) {
-        if (isWinner) {
-            wins++
-        } else {
-            losses++
-        }
-    }
-
-    fun getStatistics(): GameResult {
-        val totalGames = wins + losses
-        val winPercentage = if (totalGames > 0) (wins.toDouble() / totalGames) * 100 else 0.0
-        return GameResult(totalGames, wins, 0, losses, winPercentage)
-    }
-}
-}*/
